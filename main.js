68cf9adce1b38bf0ce1fdbb3411a4676df605f15
AimloQ <143868721+AimloQ@users.noreply.github.com>
2024-06-02 17:13:20 +0530

    sada

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+leftWrist=0;
+rightWrist=0;
+difference=0;
 function setup()
 {
     video = createCapture(VIDEO);
@@ -10,11 +15,6 @@ function setup()
     poseNet.on('pose',gotPoses);
 }
 
-function draw()
-{
-    background('#969A97');
-}
-
 function modelLoaded()
 {
     console.log('PoseNet Is Initialized!');
@@ -24,5 +24,24 @@ function (gotPoses){
     if(results.length > 0)
         {
             console.log(results);
+            noseX=results[0].pose.nose.x;
+            noseY=results[0].pose.nose.y;
+            console.log("NoseX="+noseX+""+"NoseY"+noseY);
+
+            leftWrist=results[0].pose.leftWrist.x;
+            rightWrist=results[0].pose.rightWrist.x;
+            
+            difference= leftWrist-rightWrist;
+
+            console.log("leftWristX= "+leftWrist+""+"rightWristX= "+rightWrist+""+"Difference= "+difference);
         }
     }
+
+    function draw()
+{
+    document.getElementById("square_side").innerHTML="Width And Height Of A Square Will Be= "+difference+"px";
+    background('#969A97');
+    fill('#F90093');
+    stroke('#F90093')
+    square(noseX,noseY,difference);
+}
